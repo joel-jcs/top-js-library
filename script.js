89e9b3b65c6ecf7ecd9c6d027b70d1ef807b158c
joel-jcs <joeljcs@protonmail.com>
2024-09-08 00:21:21 -0400

    added js functionality to add book to library, show the library, and remove books when respective remove button gets clicked

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+const libraryContainer = document.getElementById('library');
+const addBookBtn = document.getElementById('add-book-btn');
+const bookInputWindow = document.getElementById('book-input-window');
+const titleInput = document.getElementById('book-title');
+const authorInput = document.getElementById('book-author');
+const pagesInput = document.getElementById('book-pages');
+const submitBookBtn = document.getElementById('submit-book-btn');
+
 const myLibrary = [];
 
 function Book(title, author, pages, hasRead) {
+    this.id = null;
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -11,18 +20,73 @@ function Book(title, author, pages, hasRead) {
 }
 
 const addBookToLibrary = () => {
-    //take user’s input and store the new book objects into an array
+    let newBook = new Book(titleInput.value, authorInput.value, pagesInput.value, false);
+    newBook.index++;
+    myLibrary.push(newBook);
+    clearInputs();
+    closeBookInputWindow();
+    showLibrary();
+}
+
+const clearInputs = () => {
+    titleInput.value = "";
+    authorInput.value = "";
+    pagesInput.value = "";
 }
 
 const showLibrary = () => {
-    // loop through the myLibrary array and display each book on the page
-    //can be shown on a "table". each book can be a "card"
+    libraryContainer.innerHTML=``;
+    myLibrary.forEach((book, index) => {
+        book.id = index;
+        libraryContainer.innerHTML += `
+        <div class="book" id="book-${book.id}">
+            <span class="book-title">${book.title}</span>
+            <span class="book-author">${book.author}</span>
+            <span class="book-pages">${book.pages}</span>
+            <button class="has-read-btn">Haven't read this</button>
+            <button class="remove-btn" value="${book.id}">Remove</button>
+        </div>
+        `
+        });
+
+    const removeBtns = document.querySelectorAll('.remove-btn');
+    removeBtns.forEach(button => {
+        button.addEventListener('click', () => {
+            removeBook(button.value);
+            showLibrary();
+        });
+    });
 }
 
-// add functionality for user to click a "NEW BOOK" button and show a form where they can input the details for each book
-//remember to use event.preventDefault() to prevent the button from submitting the form.
-
-
-//each book added should have a button to remove book from the library
+const removeBook = (index) => {
+    document.getElementById(`book-${index}`).remove();
+    myLibrary.splice(index, 1);
+}
 
 //each book should have a button to set whether it's been read or not (hasRead = !hasRead).
+
+const showBookInputWindow = () => {
+    if (getComputedStyle(bookInputWindow).display === "none") {
+        bookInputWindow.style.display = "flex";
+        libraryContainer.style.filter = "blur(5px)";
+    } else {
+        return;
+    }
+}
+
+const closeBookInputWindow = (event = null) => {
+    if (event && event.target && event.target !== addBookBtn && !bookInputWindow.contains(event.target)) {
+        bookInputWindow.style.display = "none";
+        libraryContainer.style.filter = "none";
+    } else if (!event) {
+        bookInputWindow.style.display = "none";
+        libraryContainer.style.filter = "none";
+    }
+}
+
+addBookBtn.addEventListener('click', showBookInputWindow);
+submitBookBtn.addEventListener('click', event => {
+    event.preventDefault();
+    addBookToLibrary();
+});
+document.addEventListener('click', closeBookInputWindow);
